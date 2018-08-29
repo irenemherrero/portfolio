213f7eb76cd3eec24761b8ee34c6550f46194d0c
irenemherrero <irenemherrero@gmail.com>
2018-08-30 00:16:02 +0200

    header effects

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import photoMe from "../images/foto_perfil_prop.jpg"
 class Hero extends Component {
-    
-    render() { 
-        return ( 
-            <div className="hero-text-container">
-            <h1 className="font-pre-title">Más que diseño web.</h1>
-            <h2 className="big-title font-big-title">Soy Irene Mateo, Junior Front-End Developer.</h2>
-            <h2 className="font-big-title-smaller">
-            Estoy en busca de mi primera experiencia laboral como desarrolladora.</h2>
-            <h2 className="font-big-title-smaller">Descubre lo que puedo aportar a tu proyecto.</h2>
-            <img className="photoMe" src={photoMe} alt="error"/>
-            </div>
-         );
+
+    render() {
+        return (
+            <Fragment>
+                <div className="hero-text-container">
+                    <h1 className="font-pre-title">Junior Front-End Developer</h1>
+                    <h2 className="big-title font-big-title typewriter">Soy Irene Mateo.</h2>
+                    <img className="photoMe" src={photoMe} alt="error" />
+                </div>
+            </Fragment>
+
+        );
     }
 }
- 
+
 export default Hero;
