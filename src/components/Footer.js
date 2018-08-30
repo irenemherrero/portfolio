7c8d1b33df71d67b8648ae7561d1f160afeba5aa
irenemherrero <irenemherrero@gmail.com>
2018-08-30 23:38:45 +0200

    add projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,8 @@ class Footer extends Component {
     render() { 
         return ( 
             <footer className="container-footer">
-            <div className="boton-subir"> 
-            <a href="#top"> 
-            <img className="flecha-arriba" src="images/ico-arrow.svg" alt="icono para subir al menu"/>
-            </a> 
-            </div>
-            <p className="footer-line-1 font-footer-paragraph">©2017</p>
-            <p className="footer-line-2 font-footer-paragraph">we 
-            <span className="corazon">❤︎</span> components</p>
-          </footer>    
+            
+            </footer>    
          );
     }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,32 @@
 import React, { Component, Fragment } from 'react';
+import web1 from "../images/web4.png"
+import web2 from "../images/web5.png"
+import web3 from "../images/web2.png"
+import web4 from "../images/web3.png"
+import web5 from "../images/web1.png"
 
 class Projects extends Component {
     render() {
         return (
            <Fragment>
                 <div className="containerProjects">
-                    <a className="imageProject" href="">
-                        <img src="" alt=""/>
+                    <a className="imageProject" href="http://beta.adalab.es/dorcas-s3-b-geeks/#/card-generator" target="_blank">
+                        <img className="imageWeb" src={web1} alt="Error"/>
                     </a>
-                    <a className="imageProject" href="">
-                        <img src="" alt=""/>
+                    <a className="imageProject" href="http://beta.adalab.es/dorcas-s3-evaluacion-final-irenemherrero/#/" target="_blank">
+                    <img className="imageWeb" src={web2} alt="Error"/>
                     </a>
-                    <a className="imageProject" href="">
-                        <img src="" alt=""/>
+
+                    <a className="imageProject" href="http://beta.adalab.es/dorcas-s2-evaluacion-final-irenemherrero/" target="_blank">
+                    <img className="imageWeb" src={web3} alt="Error"/>
                     </a>
-                    <a className="imageProject" href="">
-                        <img src="" alt=""/>
+   
+                    <a className="imageProject" href="http://beta.adalab.es/dorcas-s2-tarjetas-martirio/" target="_blank">
+                    <img className="imageWeb" src={web4} alt="Error"/>
+                    </a>
+
+                    <a className="imageProject" href="http://beta.adalab.es/d-s1-give-me-divs/" target="_blank">
+                    <img className="imageWeb" src={web5} alt="Error"/>
                     </a>
                 </div>
            </Fragment>
