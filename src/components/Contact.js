aa307a31fdd890863f8188e1470fe485d87556c8
irenemherrero <irenemherrero@gmail.com>
2018-09-30 20:20:24 +0200

    small changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ class Contact extends Component {
         return (
             <div id="contact" className="containerContact">
                 <p className="paragraphContact paragraphTitle"> Contacto </p>
-                <p className="fontListSkill">Si crees que puedo ayudarte en tu proyecto, escribe a:</p>
+                <p className="fontListSkill">Si puedo ayudarte en tu proyecto, escribe a:</p>
                 <a className="linkMenu" href="mailto:irenemherrero@gmail.com" target="_blank"> irenemherrero@gmail.com </a>
             </div>
         );
