a64e9940521985ac167c97eb4a4d88a59f55da68
Irene Mateo Herrero <irenemherrero@gmail.com>
2021-10-15 16:57:36 +0200

    feat: solved compilation issues

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ class Contact extends Component {
             <div id="contact" className="containerContact">
                 <p className="paragraphContact paragraphTitle"> Contacto </p>
                 <p className="fontListSkill">Si quieres contactar conmigo, escribe a:</p>
-                <a className="linkMenu" href="mailto:irenemherrero@gmail.com" target="_blank"> irenemherrero@gmail.com </a>
+                <a className="linkMenu" href="mailto:irenemherrero@gmail.com" target="_blank" rel="noopener noreferrer"> irenemherrero@gmail.com </a>
             </div>
         );
     };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/main.css';
 import App from './components/App';
-import Fontawesome from '@fortawesome/fontawesome-free/css/all.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
